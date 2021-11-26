b53151eef432c4e0cab00da1cf4c27d28fbfe1e2
Riya Chatterjee <56023007+rrriiiyyyaaa@users.noreply.github.com>
2021-11-26 20:43:47 +0530

    Update day1.py
    while loop

--- day1.py
+++ day1.py
@@ -1,3 +1,16 @@
+...
+  Given an integer,n , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20 , print Not Weird
+
+
+...
+
+
+
 n = int(input().strip())
 
 if (n%2 != 0) or (n>=6 and n<=20):
