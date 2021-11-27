d8cf8d64e2497979c2f8d6790bb1b32f247ecef4
Riya Chatterjee <56023007+rrriiiyyyaaa@users.noreply.github.com>
2021-11-27 14:25:30 +0530

    Arithmetic operators

---
+++ day2.py
@@ -0,0 +1,27 @@
+'''
+   The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+'''
+
+
+#taking user input
+a = int(input())
+b = int(input())
+
+#printing
+print (a+b)
+print(a-b)
+print(a*b)
+
+
+'''
+Output-
+3
+5
+8
+-2
+15
+'''
