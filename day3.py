46e5e5c3682d39d7cf5de7f3b39763c0fc4aac4c
Riya Chatterjee <56023007+rrriiiyyyaaa@users.noreply.github.com>
2021-11-28 17:31:07 +0530

    Division

---
+++ day3.py
@@ -0,0 +1,19 @@
+a= int(input())
+b= int(input())
+
+
+
+print (a // b)
+print (a/b)
+
+...
+a=6
+b=7
+
+output-
+# division of number(floor) i.e, writtens the whole i.e. 6//7 =  1.1666666666666667= 1(whole no.)
+1
+#division(float)
+1.1666666666666667
+...
+
