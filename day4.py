5b8c3c7bf90d4544b1170237de41f7e932ab3221
Riya Chatterjee <56023007+rrriiiyyyaaa@users.noreply.github.com>
2021-11-29 17:38:56 +0530

    Print the sum of the current number and the previous number

--- day4.py
+++ day4.py
@@ -15,3 +15,16 @@ def leap(Year):
  
 
 leap(Year)  
+
+'''
+   Write a program to iterate the first 10 numbers and in each iteration, print the sum of the current and previous number.
+'''
+
+
+print('Print the sum of the current number and the previous number')
+pre_num = 0
+for i in range(1,11):
+    sum = pre_num + i
+    print('Current number=', i,"Previous number=", pre_num,'Sum = ', pre_num + i)
+    pre_num = i
+    
